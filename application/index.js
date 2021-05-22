2e465a5e477b693bf3f9b30666f58261dd516888
Stephen Tillman <askmrtillman@gmail.com>
2021-05-22 01:54:17 -0400

    implement use case to process icons; absorb partition method into use case

--- application/index.js
+++ application/index.js
@@ -1,7 +1,9 @@
 const { CollectGravatarIcons } = require('./collect-gravatar-icons');
 const { CollectTwitterIcons } = require('./collect-twitter-icons');
+const { ProcessIcons } = require('./process-icons');
 
 module.exports = {
   CollectGravatarIcons,
   CollectTwitterIcons,
+  ProcessIcons
 }
---
+++ application/process-icons.js
@@ -0,0 +1,45 @@
+const { 
+  SQSService
+} = require("avatarbox.sdk/Release/Services/sqs.service");
+
+class ProcessIcons {
+  constructor(icons){
+    this.icons = icons;
+    this.sqs = new SQSService();
+    this.gravatarIcons = [];
+    this.twitterIcons = [];
+  }
+  process(icons){
+    return this.sqs.touch(...icons);
+  }
+  partition(collection){
+    const batchSize = 10;
+    let start = 0;
+    let end = batchSize;
+    const result = [];
+    while(start < collection.length){
+      result.push(collection.slice(start, end));
+      start += batchSize;
+      end += batchSize;
+    }
+    return result;
+  }
+  async execute(){
+    const { icons } = this;
+    if(!icons || !icons.length) {
+      console.info("found 0 Gravatars");
+      return;
+    }
+
+    if(icons.length <= 10){
+      await this.process(icons);
+    } else {
+      const batches = this.partition(icons);
+      await Promise.all(
+        batches.map(this.process.bind(this))
+      );
+    }
+  }
+}
+
+module.exports = { ProcessIcons }
